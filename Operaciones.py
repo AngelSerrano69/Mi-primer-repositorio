b7b2c891cf8af5d58fbf41cbb175a7d0f57b75ed
AngelSerrano69 <123orlandocastro@gmail.com>
2026-04-30 22:31:27 -0500

    Create Operaciones.py

---
+++ Operaciones.py
@@ -0,0 +1,17 @@
+#definimos variables para trabajar con operaciones aritmeticas
+a = 25
+b = 50
+c = 30
+d = 9
+
+#realizamos realizamos las operaciones y guardamos el valor de su variable
+suma = a + b
+resta = a - b
+multiplicacion = a * b
+division = a / b
+
+#mostramos el resultado de cada operacion
+print("la suma es", suma)
+print("la resta es", resta)
+print("la multiplicacion es", multiplicacion)
+print("la division es", division)
